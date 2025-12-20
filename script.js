e98562ff62b9ac08be21e307597aad5c8d102baa
pyr <jzhangnv@gmail.com>
2025-12-20 17:00:10 -0500

    Add sketchpad with variable cell size

---
+++ script.js
@@ -0,0 +1,23 @@
+const grid = document.querySelector("#grid");
+const setGridSize = document.querySelector("#setGridSize");
+setGridSize.addEventListener('click', () => {
+    let size = parseInt(prompt('How big should a side be?'));
+    removeGrid();
+    makeGrid(size);
+});
+function makeGrid(size){
+    for (i=1;i<=(size);i++){
+        const cellY = grid.appendChild(document.createElement('div'));
+        cellY.classList.add("cellY");
+            for (j=1;j<=(size);j++){
+                const cellX = cellY.appendChild(document.createElement('div'));
+                cellX.addEventListener('mouseover', () => {
+                    cellX.style['background']='blue';
+                });
+                cellX.classList.add("cellX");
+            }
+    }
+}
+function removeGrid(){
+    (document.querySelectorAll('.cellY')).forEach((cell) => cell.remove());
+}
